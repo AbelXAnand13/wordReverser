79e72917f94eb2c34f78e7132261853479366ef9
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-12-14 16:58:53 -0800

    Initial Commit

---
+++ wordReverser/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  wordReverser
+//
+//  Created by Abel Christy Anand on 12/14/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
